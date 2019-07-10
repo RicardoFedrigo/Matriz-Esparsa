8c8f1d9c25bc73748e7729a000f0a750c1b472d3
Ricardo Fedrigo <39976915+RicardoFedrigo@users.noreply.github.com>
2019-07-10 04:05:51 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -5,15 +5,17 @@
 void main()
 {
     MatrizEsparsa* m = criarMatriz(5,5);
-    inserir(m,0,0,1);
-    inserir(m,1,1,10);
-    inserir(m,0,1,2);
-    inserir(m,0,2,3);
-    inserir(m,0,3,4);
+    // inserir(m,0,0,1);
+    // inserir(m,1,1,10);
+    // inserir(m,0,1,2);
+    // inserir(m,0,2,3);
+    // inserir(m,0,3,4);
 
     printf("%d \n",m->linhas[0]->valor);
     printf("%d \n",m->linhas[0]->direita->valor);
     printf("%d \n",m->linhas[0]->direita->direita->valor);
     printf("%d \n",m->linhas[1]->valor);
+
+    printf("%d \n",m->colunas[1]->valor);
 
 }
--- matrizEsparsa.h
+++ matrizEsparsa.h
@@ -1,4 +1,5 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 //VERS�O SEM SENTINELA
 typedef struct node{
     int linha;
@@ -9,16 +10,13 @@ typedef struct node{
 }Node;
 
 typedef struct{
-    Node** linhas;    //ponteiro para o vetor cujas c�lulas s�o ponteiros de n�s
-    Node** colunas;    //ponteiro para o vetor cujas c�lulas s�o ponteiros de n�s
-    int numLinhas;    //Quantidade de linhas da matriz
-    int numColunas;    //Quantidade de colunas da matriz
+    Node** linhas;    
+    Node** colunas;    
+    int numLinhas;    
+    int numColunas;    
 }MatrizEsparsa;
 
-MatrizEsparsa* criarMatriz(int qtdeLinhas, int qtdeColunas);
-int inserir(MatrizEsparsa* m, int linha, int coluna, int valor);
-
-MatrizEsparsa* criarMatriz(int qtdeLinhas, int qtdeColunas){ //Cria uma matriz vazia e devolve seu endere�o de mem�ria.
+MatrizEsparsa* criarMatriz(int qtdeLinhas, int qtdeColunas){
   MatrizEsparsa* m = (MatrizEsparsa*)malloc(sizeof(MatrizEsparsa));
   m->numLinhas = qtdeLinhas;
   m->numColunas = qtdeColunas;
@@ -28,58 +26,73 @@ MatrizEsparsa* criarMatriz(int qtdeLinhas, int qtdeColunas){ //Cria uma matriz v
   for(int i=0;i<qtdeColunas;i++) m->colunas[i] = NULL;
   return m;
 }
+int remover(MatrizEsparsa* m, int linha, int coluna);
+
 
 int inserir(MatrizEsparsa* m, int linhap, int colunap, int valorp){
-  
   if( linhap<0 || linhap>=m->numLinhas || colunap<0 || colunap >=m->numColunas)
     return 0;
   Node* ant = NULL;
   Node* atual = m->linhas[linhap];
-  
+  Node* aux;
   while(atual != NULL && atual->coluna<colunap){
    
     ant = atual;
     atual = atual->direita;
   }
-  if(atual != NULL && atual->coluna == colunap){
-    if(valorp == 0){
-      if(ant==NULL)
-        m->linhas[linhap] = atual->direita;
-      else
-        ant->direita = atual->direita;
-      free(atual);
+  if(atual != NULL && atual->coluna == colunap)
+  {
+    if(valorp == 0)
+   {
+  //     if(ant==NULL)
+  //     { 
+  //       m->linhas[linhap] = atual->direita;
+  //     }else{
+  //       ant->direita = atual->direita;
+  //     }free(atual);
+  //    remover(m,linhap,colunap);
     }
      else
       atual->valor = valorp;
   }
   else
   {
+          { 
+  {
       Node* novo = (Node*)malloc(sizeof(Node));
       novo->coluna = colunap;
       novo->valor = valorp;
       novo->direita = atual;
-      if(ant == NULL)
+      aux=novo;
+      if(ant == NULL){
         m->linhas[linhap] = novo;
-      else
+      }else{
         ant->direita = novo;
+      }
   }
+//IMPLEMENTACAO COLUNA 
 
+  Node* ant_coluna = NULL;
+  Node* atual_coluna = m->colunas[colunap];
+  while(atual_coluna != NULL && atual_coluna->linha<linhap){
   
+    ant_coluna = atual_coluna;
+    atual_coluna = atual_coluna->baixo;
+  }
+ 
+  if(ant_coluna == NULL){
+    m->colunas[colunap] = aux;
+  }else{
+    ant_coluna->baixo = atual_coluna ;
+  }
+  aux->baixo = atual_coluna;
 }
+void imprimir(MatrizEsparsa* m)
+{
 
-//Insere o <valor> na matriz <m> na linha <linha> e coluna <coluna>. Caso a posi��o j� exista, substitua o valor da c�lula.
-
-int remover(MatrizEsparsa* m, int linha, int coluna);
-//Remove o elemento da linha <linha> e coluna <coluna> na matriz <m> . Devolva 1 caso a remo��o seja efetivada e 0 caso contr�rio.
-
-int acessar(MatrizEsparsa* m, int linha, int coluna);
-//Devolve o valor correspondente a linha e coluna solicitada. Fa�a a valida��o dos �ndices. Caso a posi��o solicitada esteja fora do intervalo, devolva zero.
-
-
-void imprimir(MatrizEsparsa* m);  
-//(https://repl.it/@liberato/MatrizEsparsa)
-//Imprime os valores da matriz na tela. Cada linha deve ser impressa em uma linha diferente e os elementos separados por espa�o ou tabula��o. Os elementos n�o representados na matriz (valor zero), tamb�m devem ser impressos.
-
-
-void desalocar(MatrizEsparsa* m);
-//Libera toda mem�ria alocada dinamicamente para a matriz.
+  for (int i = 0; i < 8; i++)
+  {
+    
+  }
+    
+}
